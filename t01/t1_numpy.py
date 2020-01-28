87706efe5464e254a39a8a318bd9f2e2779fca2f
so051225 <maxfan051225@gmail.com>
2020-01-28 20:31:15 +0800

    comment

--- t01/t1_numpy.py
+++ t01/t1_numpy.py
@@ -11,16 +11,25 @@ import statsmodels.api as sm
 
 nsample = 100
 
+# generate an array wiht length 100 evenly
+# upper & lower bound is 0 & 10
 x = np.linspace(0,10,nsample)
+
+#
+# [0, ..., 10] -> [[1, 0], ..., [1, 10]]
 X = sm.add_constant(x)
 
+#
+# here assume the beta_constant is 1, beta_1 is 10
 beta = np.array([1, 10])
 
-e = np.random.normal(size=nsample)*100
+# multiply 100 to enlarge error
+e = np.random.normal(size = nsample) * 100
 
-y = np.dot(X, beta) + e
+# real y
+y_real = np.dot(X, beta) + e
 
-model = sm.OLS(y, X)
+model = sm.OLS(y_real, X)
 
 results = model.fit()
 
@@ -29,7 +38,7 @@ print(results.summary())
 y_fitted = results.fittedvalues
 fig, ax = plt.subplots(figsize=(8,6))
 
-ax.plot(x, y, 'o', label='data')
+ax.plot(x, y_real, 'o', label='data')
 ax.plot(x, y_fitted, 'r--', label='OLS')
 ax.legend(loc='best')
 
