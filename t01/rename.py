37d27c6a3392fc05ef5628b6246d7dd6465db8b1
so051225 <maxfan051225@gmail.com>
2020-02-11 22:48:30 +0800

    fix bugs

--- t01/rename.py
+++ t01/rename.py
@@ -1,8 +1,13 @@
 
 
 import os
-rootdir = 'xxx'
+rootdir = ''
 jar_path = 'x'
+
+file_ext_whitelist = [] # means all
+# file_ext_whitelist = ['.pdf', '.ipynb', '.nb', '.csv']
+
+to_be_moved = ''
 
 import subprocess
 #subprocess.run(["java", "-cp", jar_path, 'zhcode', '-a7', oldfile_name, newfile_name])
@@ -10,9 +15,11 @@ import subprocess
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         oldfile_name = subdir + '/' + file
-        if os.path.isfile(oldfile_name) and '.pdf' in file:    
-            newfile_name = subdir + '/' + file.replace('xxx', '')
+        # print(file)
+        filename, file_extension = os.path.splitext(file)
+        # print(file_extension)
+        if os.path.isfile(oldfile_name) and ( len(file_ext_whitelist) ==0 or (file_extension in file_ext_whitelist)):    
+            newfile_name = subdir + '/' + file.replace(to_be_moved, '')
             os.rename(oldfile_name, newfile_name)
-            # print('oldfile_name: %s, newfile_name: %s' % (oldfile_name, newfile_name))
+            #print('oldfile_name: %s, newfile_name: %s' % (oldfile_name, newfile_name))
         
-    
